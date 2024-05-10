841fc0e396bc995db3ef3a8ad9c0110d4e986407
Gabriel Moraes <moraes.madm@gmail.com>
2024-05-09 21:04:23 -0300

    Stylizes the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import data from "./data.json";
 import bandeiraImage from "./assets/bandeira.png";
 import Card from "./components/Card/Card";
+
+import "./App.scss";
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -15,11 +16,54 @@ function App() {
     }, 1000);
   }, []);
 
+  const [phrases, setPhrases] = useState([
+    "São mais de 5000 famílias desabrigadas",
+    "Ajude-nos a reconstruir",
+    "Faça a diferença hoje",
+    // Add more phrases as needed
+  ]);
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [currentPhrase, setCurrentPhrase] = useState("");
+  const [currentPhraseLength, setCurrentPhraseLength] = useState(0);
+
+  // Function to type out the current phrase
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentPhraseLength < phrases[currentPhraseIndex].length) {
+        setCurrentPhrase(
+          (prevPhrase) =>
+            prevPhrase + phrases[currentPhraseIndex].charAt(currentPhraseLength)
+        );
+        setCurrentPhraseLength((prevLength) => prevLength + 1);
+      } else {
+        clearInterval(interval);
+        setTimeout(() => {
+          setCurrentPhrase("");
+          setCurrentPhraseLength(0);
+          setCurrentPhraseIndex((prevIndex) =>
+            prevIndex === phrases.length - 1 ? 0 : prevIndex + 1
+          );
+        }, 2000); // Pause before typing the next phrase
+      }
+    }, 100); // Typing speed (milliseconds per letter)
+
+    return () => clearInterval(interval);
+  }, [currentPhrase, currentPhraseLength, currentPhraseIndex, phrases]);
+
   return (
     <div className="App">
-      <div className="title">
-        <h1>AJUDE O RS!</h1>
-        <img src={bandeiraImage} alt="Bandeira" className="bandeira" />
+      <div className="hero">
+        <div className="hero__header">
+          <h1 className="hero__title">Ajude o RS</h1>
+          <p className="hero__subtitle">
+            <span>{currentPhrase}</span>
+          </p>
+        </div>
+        <p className="hero__description">
+          Este site reune opções de instituições que estão prestando socorro às
+          vitimas das enchentes no Rio Grande do Sul. Escolha as causas que você
+          quer apoiar e ajude quem está precisando.
+        </p>
       </div>
       <div className="card-grid">
         {cards.map((card) => (
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
